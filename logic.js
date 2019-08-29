d143471bc49bca21409658c98902465ccd9140e1
Rebecca kawa <rebeccakawa@Rebeccas-MacBook-Pro.local>
2019-08-29 16:56:16 -0400

    removing extra parts and adding all the contente except logos

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
 emailButton = document.getElementById('getInTouchBtn')
 emailButton.addEventListener('click', function(){
-    window.location.href='mailto:mail@example.org';
+    window.location.href='mailto:info@sarona-partners.com';
 })
